3c5cb80ff8e395f2a8d8ff3af334bc64960844cd
shosheski <gorazdsoseski03@gmail.com>
2024-11-06 19:01:20 +0100

    update3

--- scraper.py
+++ scraper.py
@@ -1,10 +1,23 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-URL = "https://www.mse.mk/mk/stats/symbolhistory/kmb"
+URL = "https://www.mse.mk/en/stats/symbolhistory/kmb"
 
-response = requests.get(URL)
-html = response.content
-soup = BeautifulSoup(html, "html.parser")
+def fetch_issuer_codes(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    codes = []
+    dropdown = soup.find("select", {"id": "Code"})
+
+    code_pattern = re.compile(r'^[A-Za-z]+$')
+
+    for option in dropdown.find_all("option"):
+        code = option.get("value")
+        if code and code_pattern.match(code):
+            codes.append(code)
+
+    return codes
 
 
