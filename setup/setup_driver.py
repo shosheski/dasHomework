995737d8b1e4e89157ca8331a2b0e05c17e931e7
shosheski <gorazdsoseski03@gmail.com>
2024-11-08 13:06:51 +0100

    update8

--- setup/setup_driver.py
+++ setup/setup_driver.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+import os
 
-def setup_driver(url):
-    driver_path = "C://Users/goraz/Desktop/geckodriver.exe"
-    firefox_binary_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+def setup_driver(url, driver_path=None, firefox_binary_path=None):
+    if driver_path is None:
+        driver_path = os.getenv("GECKODRIVER_PATH", "geckodriver")
+    if firefox_binary_path is None:
+        firefox_binary_path = os.getenv("FIREFOX_BINARY_PATH", "firefox")
 
     options = Options()
     options.binary_location = firefox_binary_path
@@ -12,6 +15,5 @@ def setup_driver(url):
 
     driver = webdriver.Firefox(service=service, options=options)
     driver.get(url)
-
     driver.implicitly_wait(5)
     return driver
